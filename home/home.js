f772c38b68d19b5213da1ffaf43ec5c2dd83c5aa
DiogoChelles <diogochelles@gmail.com>
2026-09-11 09:26:36 -0300

    :iphone: feat: Implementa código responsivo referente à continuação do exercício em sala de aula.

--- home/home.js
+++ home/home.js
@@ -1,9 +1,31 @@
-let btnMenu = document.querySelector(".botao-menu");
-let nav = document.querySelector(".navegacao")
+//* CÓDIGO DO DANIEL
 
-btnMenu.addEventListener("click", mostrarMenu)
+// let btnMenu = document.querySelector(".botao-menu");
+// let nav = document.querySelector(".navegacao")
 
-function mostrarMenu() {
-    nav.classList.toggle("ativo")   
-}
+// btnMenu.addEventListener("click", mostrarMenu)
+
+// function mostrarMenu() {
+//     nav.classList.toggle("ativo")   
+// }
+
+//* CÓDIGO DA PROF
+
+const menu = document.getElementById("menu"); //pegando o elemento pelo ID
+
+// estamos pegando o elemento de forma genérica - pode ser classe (precisa trazer o ponto) ou pode ser ID (precisa trazer a hashtag)
+const navMenu = document.querySelector(".navegacao")
+
+//menu.addEventListener("click", () => {}) //opção com arrow function -> função lambda -> é a mesma coisa que a função abaixo
+
+menu.addEventListener("click", function() {
+
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+    else {
+        navMenu.className = "navegacao"
+    }
+})
+
 
